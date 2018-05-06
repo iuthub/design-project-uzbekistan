44f9d0c2ee6d945d6ce608724734c3331cefec47
koinot-markazi <mahkamov4th@gmail.com>
2018-05-06 14:54:58 +0500

    home page

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,27 @@
-$(function() {
+$(window).scroll(function(){
+  var scroll = $(window).scrollTop();
+  var windowHeight = $(window).height();
 
-	// Custom JS
-
+  if(scroll >= windowHeight){
+    $(".navbar").addClass("light-nav");
+    $(".navbar").removeClass("transparent-nav");
+    $('.img_logo').attr('src', 'img/logo_uzb_grey.svg');
+  }
+  else{
+    $(".navbar").addClass("transparent-nav");
+    $(".navbar").removeClass("light-nav");
+    $('.img_logo').attr('src', 'img/logo_uzb.svg');
+  }
 });
+$('.discover').click(function(){
+  $('html, body').animate({
+    scrollTop:$('.home-second').position().top
+  }, 1000);
+});
+
+/*MMENU for Bootstrap*/
+// $(document).ready(function() {
+//    $(".navbar-collapse").mmenu({
+//       wrappers: ["bootstrap4"]
+//    });
+// });
