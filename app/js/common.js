9082e238edf39c40bc7526c6fdbdb19562c54b47
koinot-markazi <mahkamov4th@gmail.com>
2018-05-07 16:12:37 +0500

    All pages almost done
    All pages
    
    Co-Authored-By: dilorom-alieva <dilorom-alieva@users.noreply.github.com>

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,6 @@
 $(window).scroll(function(){
   var scroll = $(window).scrollTop();
-  var windowHeight = $(window).height();
+  var windowHeight = $(window).height()-50;
 
   if(scroll >= windowHeight){
     $(".navbar").addClass("light-nav");
@@ -27,7 +27,43 @@ $('.pause').click(function(){
   $('.player').trigger('pause');
   $('.play').css('display', 'block');
   $('.pause').css('display', 'none');
-})
+});
+
+$(function() {
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+});
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop:true,
+    // nav:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },            
+        960:{
+            items:5
+        },
+        1200:{
+            items:6
+        }
+    }
+});
+
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
 
 /*MMENU for Bootstrap*/
 // $(document).ready(function() {
